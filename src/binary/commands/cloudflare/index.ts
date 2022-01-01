54088c6ea9c77cebef2b6475d9ebb63afef55e60
Mikkel RINGAUD <vexitofficial@gmail.com>
2022-01-01 21:00:19 +0100

    Creating help message to test out the CLI

--- src/binary/commands/cloudflare/index.ts
+++ src/binary/commands/cloudflare/index.ts
@@ -1,3 +1,20 @@
+const helpMessage = `
+Help: Cloudflare Provider - CLI
+
+  * update [ZONE_ID] [DNS_RECORD_ID]
+    [--ip X.X.X.X] => defaults to the current public IP.
+    [--dns-type A|AAAA] => defaults to A. Only A and AAA are currently supported.
+`;
+
 export default async function handleCommand (args: string[]) {
-  return args;
+  const commandName = args[0];
+  const commandArguments = args.slice(1);
+
+  switch (commandName) {
+  case "update":
+    console.log(commandArguments);
+    break;
+  default:
+    return console.info(helpMessage);
+  }
 }
